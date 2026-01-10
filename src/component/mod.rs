56f29f6cd4c2a4db0e2f6ea5931d02b73b2dfeee
quackitsquinn <quinnweismiller@gmail.com>
2026-01-09 22:38:57 -0600

    ComponentStore is now Send + Sync

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -3,29 +3,34 @@ use std::{
     cell::{OnceCell, Ref},
     collections::HashMap,
     fmt::Debug,
-    rc::Rc,
+    sync::{Arc, OnceLock},
 };
 
 pub mod handles;
 mod resource;
 
+use parking_lot::{MappedRwLockReadGuard, MappedRwLockWriteGuard};
 use resource::ResourceNode;
 use rustc_hash::FxBuildHasher;
 
 type ResourceMap = HashMap<TypeId, ResourceNode, FxBuildHasher>;
 
+pub type ComponentReadGuard<'a, T> = MappedRwLockReadGuard<'a, T>;
+pub type ComponentWriteGuard<'a, T> = MappedRwLockWriteGuard<'a, T>;
+
 /// A database for storing components of various types.
+#[derive(Default)]
 pub struct ComponentStore {
-    map: Rc<ResourceMap>,
-    public_ref: Rc<OnceCell<ComponentStore>>,
+    map: Arc<ResourceMap>,
+    public_ref: Arc<OnceLock<ComponentStore>>,
 }
 
 impl ComponentStore {
     /// Creates a new, empty component database.
     pub fn new() -> Self {
         Self {
-            map: Rc::new(HashMap::default()),
-            public_ref: Rc::new(OnceCell::new()),
+            map: Arc::new(HashMap::default()),
+            public_ref: Arc::new(OnceLock::new()),
         }
     }
 
@@ -38,14 +43,14 @@ impl ComponentStore {
     }
 
     /// Function for internal use to get the resource map. (must not be exposed publicly)
-    fn get_map(&self) -> &Rc<ResourceMap> {
+    fn get_map(&self) -> &Arc<ResourceMap> {
         &self.map
     }
 
     /// Inserts a component into the database.
     ///
     /// There must be no other references to the database when calling this method.
-    pub fn insert<T: 'static>(&mut self, component: T) -> ComponentHandle<T> {
+    pub fn insert<T: 'static + Send + Sync>(&mut self, component: T) -> ComponentHandle<T> {
         if self.map.contains_key(&TypeId::of::<T>()) {
             panic!(
                 "Component of type {} already exists in State",
@@ -54,7 +59,7 @@ impl ComponentStore {
         }
 
         let mut_map =
-            Rc::get_mut(&mut self.map).expect("Cannot insert component into shared State");
+            Arc::get_mut(&mut self.map).expect("Cannot insert component into shared State");
 
         mut_map.insert(TypeId::of::<T>(), ResourceNode::new(component));
         self.handle_for::<T>()
@@ -104,12 +109,12 @@ impl<T> ComponentHandle<T> {
     }
 
     /// Gets a reference to the component.
-    pub fn get(&self) -> Ref<'_, T> {
+    pub fn get(&self) -> ComponentReadGuard<'_, T> {
         self.handle.get::<T>()
     }
 
     /// Gets a mutable reference to the component.
-    pub fn get_mut(&self) -> std::cell::RefMut<'_, T> {
+    pub fn get_mut(&self) -> ComponentWriteGuard<'_, T> {
         self.handle.get_mut::<T>()
     }
 }
@@ -134,19 +139,19 @@ impl<T> Clone for ComponentHandle<T> {
 pub struct ComponentStoreHandle {
     // TODO: Figure out a way to optimize this into a single pointer sized field.
     // This is gonna need some unsafe code and weird pointer tagging so this is a later task.
-    handle: OnceCell<Rc<ResourceMap>>,
-    global_handle: Rc<OnceCell<ComponentStore>>,
+    handle: OnceLock<Arc<ResourceMap>>,
+    global_handle: Arc<OnceLock<ComponentStore>>,
 }
 
 impl ComponentStoreHandle {
     pub fn new(component_map: &ComponentStore) -> Self {
         Self {
-            handle: OnceCell::new(),
+            handle: OnceLock::new(),
             global_handle: component_map.public_ref.clone(),
         }
     }
 
-    fn get_map(&self) -> &Rc<ResourceMap> {
+    fn get_map(&self) -> &Arc<ResourceMap> {
         self.handle.get_or_init(|| {
             let global = self
                 .global_handle
@@ -169,18 +174,20 @@ impl Debug for ComponentStoreHandle {
 }
 
 mod get_impls {
-    use crate::component::{ComponentStore, ComponentStoreHandle};
+    use crate::component::{
+        ComponentReadGuard, ComponentStore, ComponentStoreHandle, ComponentWriteGuard,
+    };
 
     macro_rules! impl_get {
         () => {
             /// Gets a reference to a component of the specified type.
-            pub fn get_checked<T: 'static>(&self) -> Option<std::cell::Ref<'_, T>> {
+            pub fn get_checked<T: 'static>(&self) -> Option<ComponentReadGuard<'_, T>> {
                 let component = self.get_map().get(&std::any::TypeId::of::<T>())?;
                 Some(unsafe { component.downcast_ref_unchecked() })
             }
 
             /// Gets a reference to a component of the specified type.
-            pub fn get<T: 'static>(&self) -> std::cell::Ref<'_, T> {
+            pub fn get<T: 'static>(&self) -> ComponentReadGuard<'_, T> {
                 if let Some(component) = self.get_checked::<T>() {
                     component
                 } else {
@@ -192,13 +199,13 @@ mod get_impls {
             }
 
             /// Gets a mutable reference to a component of the specified type.
-            pub fn get_mut_checked<T: 'static>(&self) -> Option<std::cell::RefMut<'_, T>> {
+            pub fn get_mut_checked<T: 'static>(&self) -> Option<ComponentWriteGuard<'_, T>> {
                 let component = self.get_map().get(&std::any::TypeId::of::<T>())?;
                 Some(unsafe { component.downcast_mut_unchecked() })
             }
 
             /// Gets a mutable reference to a component of the specified type.
-            pub fn get_mut<T: 'static>(&self) -> std::cell::RefMut<'_, T> {
+            pub fn get_mut<T: 'static>(&self) -> ComponentWriteGuard<'_, T> {
                 if let Some(component) = self.get_mut_checked::<T>() {
                     component
                 } else {
--- src/component/resource.rs
+++ src/component/resource.rs
@@ -1,19 +1,23 @@
-use std::cell::RefCell;
+use parking_lot::{
+    MappedRwLockReadGuard, MappedRwLockWriteGuard, RwLock, RwLockReadGuard, RwLockWriteGuard,
+};
+
+use crate::component::{ComponentReadGuard, ComponentWriteGuard};
 
 /// Internal representation of a resource.
 /// This contains various metadata about the resource.
 #[derive(Debug)]
 pub struct ResourceNode {
     pub type_name: &'static str,
-    pub data: RefCell<Box<dyn std::any::Any>>,
+    pub data: RwLock<Box<dyn std::any::Any + Send + Sync>>,
 }
 
 impl ResourceNode {
     /// Creates a new ResourceNode from the given data.
-    pub fn new<T: 'static + std::any::Any>(data: T) -> Self {
+    pub fn new<T: 'static + std::any::Any + Send + Sync>(data: T) -> Self {
         Self {
             type_name: std::any::type_name::<T>(),
-            data: RefCell::new(Box::new(data)),
+            data: RwLock::new(Box::new(data)),
         }
     }
 
@@ -21,8 +25,8 @@ impl ResourceNode {
     ///
     /// # Safety
     /// The caller must ensure that the type T matches the actual type of the resource.
-    pub unsafe fn downcast_ref_unchecked<T: 'static>(&self) -> std::cell::Ref<'_, T> {
-        std::cell::Ref::map(self.data.borrow(), |b| {
+    pub unsafe fn downcast_ref_unchecked<T: 'static>(&self) -> ComponentReadGuard<'_, T> {
+        RwLockReadGuard::map(self.data.read(), |b| {
             // Currently we do only use downcast_ref, but in the future this might be turned into a manual pointer cast for performance reasons.
             // Don't make safety promises you can't keep!
             b.downcast_ref::<T>()
@@ -34,8 +38,8 @@ impl ResourceNode {
     ///
     /// # Safety
     /// The caller must ensure that the type T matches the actual type of the resource.
-    pub unsafe fn downcast_mut_unchecked<T: 'static>(&self) -> std::cell::RefMut<'_, T> {
-        std::cell::RefMut::map(self.data.borrow_mut(), |b| {
+    pub unsafe fn downcast_mut_unchecked<T: 'static>(&self) -> ComponentWriteGuard<'_, T> {
+        RwLockWriteGuard::map(self.data.write(), |b| {
             // Currently we do only use downcast_mut, but in the future this might be turned into a manual pointer cast for performance reasons.
             // Don't make safety promises you can't keep!
             b.downcast_mut::<T>()
