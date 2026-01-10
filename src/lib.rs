3dbc411116ad77456ada5520553cf3b392a96d92
quackitsquinn <quinnweismiller@gmail.com>
2026-01-09 23:08:38 -0600

    Export most of the internal crates

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,17 @@ use std::sync::Arc;
 
 use glam::Vec3;
 
+pub use anyhow;
+pub use bytemuck;
+pub use glam;
+pub use parking_lot;
+pub use wgpu;
+pub use winit;
+
+// even though the project im using this for is using tokio, smol is more lightweight and fits better with the rest of kiwi's design
+// (game with heavy use of networking.. very very excited to work on this!)
+pub use smol;
+
 /// A read-only string type.
 pub type ReadOnlyString = Arc<str>;
 /// A read-only slice type.
